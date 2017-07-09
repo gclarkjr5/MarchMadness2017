bb2996c8fe0718bd4f23492c6e1c80ad1eefd1a0
Gary Clark <Gary Clark>
2017-07-09 11:07:41 -0500

    Fixed a few small errors

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-const port = process.env.port || 8000;
+const port = process.env.PORT || 8000;
+
+// app.set(`port`, (process.env.PORT || 8000))
 
 const csvWork = require(`./csvWork`)
 
